d23fdaabde24e0a169bb1d7aff722e7756f42a62
Joao <jmmsp2003@hotmail.com>
2024-03-09 00:58:02 -0300

    Protótipo finalizado

---
+++ src/scripts/scrollReveal.js
@@ -0,0 +1,40 @@
+ScrollReveal().reveal(".bannerGradient", {
+  duration: 2000,
+  distance: "100px",
+  easing: "ease",
+  reset: true,
+});
+ScrollReveal().reveal(".cardsContainer", {
+  duration: 2000,
+  distance: "5px",
+  easing: "ease",
+  reset: false,
+});
+ScrollReveal().reveal(".contactDestac__image", {
+  duration: 1000,
+  distance: "10px",
+  easing: "ease",
+  reset: true,
+});
+ScrollReveal().reveal(".contactDestac__image", {
+  origin: "left",
+  distance: "100px",
+  duration: 1000,
+  easing: "ease",
+  reset: false,
+});
+ScrollReveal().reveal(".companyName", {
+  origin: "right",
+  distance: "100px",
+  duration: 1000,
+  easing: "ease",
+  reset: false,
+});
+ScrollReveal().reveal(".contact_whatsapp", {
+  origin: "right",
+  distance: "100px",
+  duration: 1000,
+  easing: "ease",
+  reset: false,
+});
+
